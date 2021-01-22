e6a1927ce4c2ac3ff21ae90e1fe48fa72045d445
Irineu Junior <irineu@mixd.com.br>
2021-01-22 15:10:36 -0300

    feat(faqs) desabilita a imagem da categoria por padrão

--- src/config/upload-configs.php
+++ src/config/upload-configs.php
@@ -2,24 +2,24 @@
 
 return [
     'faqs-categories' => [
-        'image' => [ //nome do campo
-            'label' => 'imagem', //label do campo
-            'multiple' => false, //se permite o upload multiplo
-            'faker_dir' => false, #database_path('faker/faqs-categories/image'),
-            'sources' => [
-                [
-                    'conversion' => 'min-width-1366',
-                    'media' => '(min-width: 1366px)',
-                    'width' => 938,
-                    'height' => 680,
-                ],
-                [
-                    'conversion' => 'min-width-1280',
-                    'media' => '(min-width: 1280px)',
-                    'width' => 776,
-                    'height' => 565,
-                ],
-            ],
-        ],
+//        'image' => [ //nome do campo
+//            'label' => 'imagem', //label do campo
+//            'multiple' => false, //se permite o upload multiplo
+//            'faker_dir' => false, #database_path('faker/faqs-categories/image'),
+//            'sources' => [
+//                [
+//                    'conversion' => 'min-width-1366',
+//                    'media' => '(min-width: 1366px)',
+//                    'width' => 1024, // 16:9
+//                    'height' => 576,
+//                ],
+//                [
+//                    'conversion' => 'min-width-1280',
+//                    'media' => '(min-width: 1280px)',
+//                    'width' => 776,
+//                    'height' => 437,
+//                ],
+//            ],
+//        ],
     ],
 ];
