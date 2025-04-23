f9946bbae0c798f341871679259887aa5d55fd6d
Carlos Tamagawa <carlos@mixd.com.br>
2025-04-23 11:07:07 -0300

    fix(menu) ajusta o visible do menu

--- src/Http/Components/Aside/Faq.php
+++ src/Http/Components/Aside/Faq.php
@@ -4,6 +4,8 @@ namespace Agenciafmd\Faqs\Http\Components\Aside;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
+use Illuminate\Support\Facades\Gate;
+use Agenciafmd\Faqs\Models\Faq as FaqModel;
 
 class Faq extends Component
 {
@@ -23,7 +25,7 @@ class Faq extends Component
         $this->label = __(config('admix-faqs.name'));
         $this->url = route('admix.faqs.index');
         $this->active = request()?->currentRouteNameStartsWith('admix.faqs');
-        $this->visible = true;
+        $this->visible = Gate::allows('view', FaqModel::class);
 
         return view('admix::components.aside.item');
     }
